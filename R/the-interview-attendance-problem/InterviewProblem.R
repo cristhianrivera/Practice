e59a36f8ad2cfbb2ec32e77cfb25308202b5f1cf
Jimenez <a688291@cis.neustar.com>
2018-04-10 18:06:03 +0200

    first final version

--- R/the-interview-attendance-problem/InterviewProblem.R
+++ R/the-interview-attendance-problem/InterviewProblem.R
@@ -10,74 +10,60 @@ InterviewData <- as.data.frame(InterviewData)
 
 ##First look at the output
 
-InterviewData<- InterviewData %>% 
-  mutate(Observed.Attendance =trimws(tolower(Observed.Attendance)))
+
+InterviewData <- InterviewData %>% dplyr::mutate(Observed.Attendance = trimws(tolower(Observed.Attendance)))
+InterviewData <- InterviewData %>% filter(Observed.Attendance != "" ) #just to erase the last row
+byAttendance <- InterviewData %>% group_by(Observed.Attendance) %>% summarise(count = n()) %>% mutate(Freq = count / sum(count))
 
 
-byAttendance <- InterviewData%>%
-  group_by(Observed.Attendance)%>%
-  summarise(count = n())%>%
-  mutate(Freq = count / sum(count))
 
-ggplot(byAttendance, aes(x = "Output", y = Freq, fill = Observed.Attendance))+
-  geom_bar(stat="identity")+
-  geom_text(aes(label = round(Freq, 2)), position = position_stack(vjust = 0.5))
-  
+ggplot2::ggplot(byAttendance, aes(x = "Output", y = Freq, fill = Observed.Attendance))+ ggplot2::geom_bar(stat="identity")+geom_text(aes(label = round(Freq, 2)), position = position_stack(vjust = 0.5))
 
-
-#by location
 
 InterviewData$Location<-trimws(tolower(InterviewData$Location))
-filter(InterviewData, Location == "gurgaonr")
-
+#To correct a typo 
 InterviewData<- InterviewData %>% 
-                  mutate(Location = ifelse(
-                    Location == "gurgaonr" , 
-                    "gurgaon", 
-                    Location))
+  mutate(Location = ifelse(
+  Location == "gurgaonr" , 
+  "gurgaon", 
+  Location))
 
 byLocation <- InterviewData%>%
   group_by(Location)%>%
   summarise(count=n())
 
 ggplot(data = byLocation) + 
-  geom_col(mapping = aes(x = Location, y = count))
+geom_col(mapping = aes(x = Location, y = count))
 
-
-#by industry
 
 byIndustry <- InterviewData%>%
-  group_by(Industry)%>%
-  summarise(count = n())%>%
-  mutate(freq = count / sum(count))
+group_by(Industry)%>%
+summarise(count = n())%>%
+mutate(Freq = count / sum(count))
 
 ggplot(byIndustry)+
-  geom_col(aes(x=Industry,y=freq))
+geom_col(aes(x=Industry,y=Freq))+
+theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-
-#by Interview.Venue
 
 byInterviewVenue <- InterviewData%>%
-  group_by(Interview.Venue)%>%
-  summarise(count = n())%>%
-  mutate(freq = count / sum(count))
+group_by(Interview.Venue)%>%
+summarise(count = n())%>%
+mutate(freq = count / sum(count))
 
 ggplot(byInterviewVenue)+
-  geom_col(aes(x=Interview.Venue,y=freq))
+geom_col(aes(x=Interview.Venue,y=freq))
 
 
-
-# by Nature.of.Skillset
 byNatureSkillset <- InterviewData%>%
-  group_by(Nature.of.Skillset)%>%
-  summarise(count = n())%>%
-  mutate(freq = count / sum(count))
+group_by(Nature.of.Skillset)%>%
+summarise(count = n())%>%
+mutate(freq = count / sum(count))
 
 ggplot(byNatureSkillset)+
-  geom_col(aes(x = Nature.of.Skillset,y=freq))+
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+geom_col(aes(x = Nature.of.Skillset,y=freq))+
+theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-#---- create groups of skillsets
 
 InterviewData<- InterviewData %>% 
   mutate(SkillsetGroup = ifelse(
@@ -139,76 +125,77 @@ InterviewData<- InterviewData %>%
     "production", 
     SkillsetGroup ))
 
-InterviewData<- InterviewData %>% 
-  mutate(Can.I.Call.you.three.hours.before.the.interview.and.follow.up.on.your.attendance.for.the.interview = ifelse(
-    grepl("no", tolower(Can.I.Call.you.three.hours.before.the.interview.and.follow.up.on.your.attendance.for.the.interview)), 
-    "no", 
-    "yes" ))
 
-#Let's see how it looks now
 
 byNatureSkillset <- InterviewData%>%
-  group_by(SkillsetGroup)%>%
-  summarise(count = n())%>%
-  mutate(Freq = count / sum(count))
+group_by(SkillsetGroup)%>%
+summarise(count = n())%>%
+mutate(Freq = count / sum(count))
 
 ggplot(byNatureSkillset)+
-  geom_col(aes(x = SkillsetGroup,y = Freq))+
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+geom_col(aes(x = SkillsetGroup,y = Freq))+
+theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 
-#Select the variables that we would like to introduce into our model
+
+InterviewData<- InterviewData %>% 
+mutate(Can.I.Call.you.three.hours.before.the.interview.and.follow.up.on.your.attendance.for.the.interview = ifelse(
+grepl("no", tolower(Can.I.Call.you.three.hours.before.the.interview.and.follow.up.on.your.attendance.for.the.interview)), 
+"no", 
+"yes" ))
 
 InterviewData <- InterviewData%>%
-  mutate(Observed.AttendanceOI = ifelse(Observed.Attendance == "yes", 1 , 0 ))
+mutate(Observed.AttendanceOI = ifelse(Observed.Attendance == "yes", 1 , 0 ))
 
-WOETable(InterviewData$Gender, InterviewData$Observed.AttendanceOI )
-
-WOETable(InterviewData$Industry, InterviewData$Observed.AttendanceOI)#YES
-
-WOETable(InterviewData$SkillsetGroup, InterviewData$Observed.AttendanceOI)#YES
-
-WOETable(InterviewData$Can.I.Call.you.three.hours.before.the.interview.and.follow.up.on.your.attendance.for.the.interview, InterviewData$Observed.AttendanceOI)
-
-#This is because there's a problem, so we can transform this variable to have somethign understandable for the machine
 InterviewData <- InterviewData%>%
-  mutate(CallBeforeInterview = ifelse(Can.I.Call.you.three.hours.before.the.interview.and.follow.up.on.your.attendance.for.the.interview == "yes", 1 , 0 ))
+mutate(CallBeforeInterview = ifelse(Can.I.Call.you.three.hours.before.the.interview.and.follow.up.on.your.attendance.for.the.interview == "yes", 1 , 0 ))
 
 InterviewData$CallBeforeInterview <- as.factor(InterviewData$CallBeforeInterview)
 
-WOETable(InterviewData$CallBeforeInterview, InterviewData$Observed.AttendanceOI)#No
-
-
-WOETable(InterviewData$Marital.Status, InterviewData$Observed.AttendanceOI)#No
-
-WOETable(InterviewData$CallBeforeInterview, InterviewData$Observed.AttendanceOI)#No
-
-#This is because there's a problem, so we can transform this variables to have something understandable for the machine
 InterviewData <- InterviewData %>%
-  mutate(Expected.Attendance = ifelse(tolower(Expected.Attendance)=="yes", "yes", "no"))
+mutate(Expected.Attendance = ifelse(tolower(Expected.Attendance)=="yes", "yes", "no"))
 
 InterviewData <- InterviewData %>%
-  mutate(ExpectedAttendanceIO = ifelse(Expected.Attendance=="yes", 1, 0))
+mutate(ExpectedAttendanceIO = ifelse(Expected.Attendance=="yes", 1, 0))
 
 InterviewData$ExpectedAttendanceIO <- as.factor(InterviewData$ExpectedAttendanceIO)
 
-WOETable(InterviewData$ExpectedAttendanceIO, InterviewData$Observed.AttendanceOI)#yes
+InterviewData$SkillsetGroup <- as.factor(InterviewData$SkillsetGroup)
 
-sum(WOETable(InterviewData$Position.to.be.closed, InterviewData$Observed.AttendanceOI)$IV)#maybe
+InterviewData$Marital.Status <- as.factor(InterviewData$Marital.Status)
 
-WOETable(InterviewData$Location, InterviewData$Observed.AttendanceOI)#NO
+InterviewData$Location <- as.factor(InterviewData$Location)
+
+
+
+sum(WOETable( InterviewData$Industry, InterviewData$Observed.AttendanceOI)$IV[-1])#YES
+
+sum(WOETable(InterviewData$SkillsetGroup, InterviewData$Observed.AttendanceOI)$IV)#YES
+
+sum(WOETable(InterviewData$CallBeforeInterview, InterviewData$Observed.AttendanceOI)$IV)#No
+
+sum(WOETable(InterviewData$Marital.Status, InterviewData$Observed.AttendanceOI)$IV[-1])#No
+
+sum(WOETable(InterviewData$CallBeforeInterview, InterviewData$Observed.AttendanceOI)$IV)#No
+
+sum(WOETable(InterviewData$ExpectedAttendanceIO, InterviewData$Observed.AttendanceOI)$IV)#yes
+
+sum(WOETable(InterviewData$Position.to.be.closed, InterviewData$Observed.AttendanceOI)$IV[-1])#maybe
+
+sum(WOETable(InterviewData$Location, InterviewData$Observed.AttendanceOI)$IV)#maybe
+
+
 
 
 ##########First version of the model, just to have a benchmark with one variable
 MOD1_InterviewData <- InterviewData%>%
   select(Observed.AttendanceOI,ExpectedAttendanceIO)%>%
   filter(is.na(ExpectedAttendanceIO)== FALSE)
-  
+
 mod1 <- glm(Observed.AttendanceOI ~ ExpectedAttendanceIO , 
             data = MOD1_InterviewData, 
             family = binomial("logit"))
 
-summary(mod1)
 
 MOD1_InterviewData <- MOD1_InterviewData%>%
   mutate(prediction = predict(mod1, MOD1_InterviewData$Expected.AttendanceIO, type="response"))
@@ -218,9 +205,7 @@ optCutOff <- optimalCutoff(MOD1_InterviewData$Observed.AttendanceOI, MOD1_Interv
 plotROC(MOD1_InterviewData$Observed.AttendanceOI, MOD1_InterviewData$prediction)
 confusionMatrix(MOD1_InterviewData$Observed.AttendanceOI, MOD1_InterviewData$prediction, threshold = optCutOff)
 
-
 ##########Second version of the model
-
 
 MOD2_InterviewData <- InterviewData%>%
   select(Observed.AttendanceOI,ExpectedAttendanceIO,SkillsetGroup )%>%
@@ -234,8 +219,6 @@ mod2 <- glm(Observed.AttendanceOI ~ ExpectedAttendanceIO + SkillsetGroup,
             data = MOD2_InterviewData, 
             family = binomial("logit"))
 
-summary(mod2)
-
 dfPredict <- select(MOD2_InterviewData, ExpectedAttendanceIO, SkillsetGroup)
 
 
@@ -243,48 +226,8 @@ MOD2_InterviewData <- MOD2_InterviewData%>%
   mutate(prediction = predict(mod2, dfPredict, type = "response"))
 
 optCutOff <- optimalCutoff(MOD1_InterviewData$Observed.AttendanceOI, MOD2_InterviewData$prediction)[1] 
-
 plotROC(MOD2_InterviewData$Observed.AttendanceOI, MOD2_InterviewData$prediction)
 confusionMatrix(MOD2_InterviewData$Observed.AttendanceOI, MOD2_InterviewData$prediction, threshold = optCutOff)
-
-
-##########Third version of the model (with all the variables that we considered "good")
-
-
-MOD_InterviewData <- InterviewData%>%
-  select(Observed.AttendanceOI,ExpectedAttendanceIO,SkillsetGroup, Industry )%>%
-  filter(is.na(ExpectedAttendanceIO)== FALSE)%>%
-  filter(is.na(SkillsetGroup)== FALSE)%>%
-  filter(is.na(Industry)== FALSE)
-
-MOD_InterviewData$ExpectedAttendanceIO <- as.factor(MOD_InterviewData$ExpectedAttendanceIO)
-MOD_InterviewData$SkillsetGroup <- as.factor(MOD_InterviewData$SkillsetGroup)
-MOD_InterviewData$Industry <- as.factor(MOD_InterviewData$Industry)
-
-mod_fine <- glm(Observed.AttendanceOI ~ ExpectedAttendanceIO + SkillsetGroup + Industry, 
-            data = MOD_InterviewData, 
-            family = binomial("logit"))
-
-summary(mod_fine)
-
-dfPredict <- select(MOD_InterviewData, ExpectedAttendanceIO, SkillsetGroup, Industry)
-#dfPredict %>% add_predictions(mod2)
-
-MOD_InterviewData <- MOD_InterviewData%>%
-  mutate(prediction = predict(mod_fine, dfPredict, type = "response"))
-
-optCutOff <- optimalCutoff(MOD_InterviewData$Observed.AttendanceOI, MOD_InterviewData$prediction)[1] 
-
-plotROC(MOD_InterviewData$Observed.AttendanceOI, MOD_InterviewData$prediction)
-confusionMatrix(MOD_InterviewData$Observed.AttendanceOI, MOD_InterviewData$prediction, threshold = optCutOff)
-
-
-
-
-
-colnames(InterviewData)
-
-
 
 
 
